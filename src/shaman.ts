ff0c3e2cf23b5fb2b8a1910f29df68bc12b23885
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-12-01 21:04:54 +0100

    shaman incitations

---
+++ src/shaman.ts
@@ -0,0 +1,68 @@
+// faith
+// DONE 0
+// 20
+// 40
+// 60
+// 80
+
+
+export default {
+  shaman: [
+
+    {
+      trust: 0,
+      faith: 0,
+      texts: [``, ``],
+    },
+    {
+      trust: 25,
+      faith: 0,
+      texts: [``, ``],
+    },
+    {
+      trust: 50,
+      faith: 0,
+      texts: [``, ``],
+    },
+    {
+      trust: 75,
+      faith: 0,
+      texts: [``, ``],
+    },
+
+
+    {
+      trust: 0,
+      faith: 0,
+      texts: [`Poor crazy soul. What have you done? Our people do not know who to follow.
+        They are lost. Show them the lights of our Gods.
+        Sacrify many to avoid our souls to be lost forever. May the Gods forgive our sins.`,
+      ],
+    },
+    {
+      trust: 25,
+      faith: 0,
+      texts: [`Jarl. Are you blind? You have to act. Gods abandon us. Last events are the proof you needed.
+        Time has come to sacrifice the bravest of us. Choose wisely.`
+      ],
+    },
+    {
+      trust: 50,
+      faith: 0,
+      texts: [`Dear Jarl. Despite your unbelievable efforts, it would be a pity if the Gods abandon us.
+        Don't forget our Gods are still watching us. Show them we still have faith.
+        You're on a great way to lead our people to glory.`,
+      ],
+    },
+    {
+      trust: 75,
+      faith: 0,
+      texts: [`Your greatness. A messenger of the Gods like you guides our people to the path of glory.
+        Gods sent you signs. Even a small offering would please them.
+        An old man, a diseased widow. Please my great Jarl...`
+      ],
+    },
+
+
+  ]
+}
