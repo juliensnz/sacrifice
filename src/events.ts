dbb3dce803f72a4526467ecadb4c955aaac64a47
Adrien Pétremann <hello@grena.fr>
2018-12-01 18:03:19 +0100

    Add events ts

---
+++ src/events.ts
@@ -0,0 +1,14 @@
+export default {
+  events: [
+    {
+      facts: [
+        'foo',
+        'bar'
+      ],
+      consequences: [
+        {consequence: 'fobarbaz', coef: -3},
+        {consequence: 'fobarbolum', coef: -7}
+      ]
+    }
+  ]
+}
