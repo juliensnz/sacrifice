e148e79cd450d816057379b6f7b091b5d98367b2
Adrien Pétremann <hello@grena.fr>
2018-12-01 23:02:51 +0100

    Update shaman structure

--- src/shaman.ts
+++ src/shaman.ts
@@ -6,108 +6,104 @@
 
 export default {
   shaman: [
-    {
-      trust: 'chaotic',
-      faith: 'chaotic',
-      texts: [``, ``],
-    },
-    {
-      trust: 'bad',
-      faith: 'chaotic',
-      texts: [``, ``],
-    },
-    {
-      trust: 'good',
-      faith: 'chaotic',
-      texts: [``, ``],
-    },
-    {
-      trust: 'loyal',
-      faith: 'chaotic',
-      texts: [``, ``],
-    },
+    // {
+    //   trust: 'chaotic',
+    //   faith: 'chaotic',
+    //   text: ``,
+    // },
+    // {
+    //   trust: 'bad',
+    //   faith: 'chaotic',
+    //   text: ``,
+    // },
+    // {
+    //   trust: 'good',
+    //   faith: 'chaotic',
+    //   text: ``,
+    // },
+    // {
+    //   trust: 'loyal',
+    //   faith: 'chaotic',
+    //   text: ``,
+    // },
 
 
     {
       trust: 'chaotic',
       faith: 'bad',
-      texts: [``, ``],
+      text: ``,
     },
     {
       trust: 'bad',
       faith: 'bad',
-      texts: [``, ``],
+      text: ``,
     },
     {
       trust: 'good',
       faith: 'bad',
-      texts: [``, ``],
+      text: ``,
     },
     {
       trust: 'loyal',
       faith: 'bad',
-      texts: [``, ``],
+      text: ``,
     },
     {
       trust: 'chaotic',
       faith: 'loyal',
-      texts: [`Poor insignificant thing. Gods talked to me. You don't have what's needed to be a Jarl.
+      text: `Poor insignificant thing. Gods talked to me. You don't have what's needed to be a Jarl.
         I don't understand why but they give you a last chance. Sacrifice your people now.
         Or yourself, if you have what it takes...`
-      ],
     },
     {
       trust: 'bad',
       faith: 'loyal',
-      texts: [`I hear whispers. People doubt you can govern them. It would be...
+      text: `I hear whispers. People doubt you can govern them. It would be...
         Sad, if something happened to you. Do what you have to do.
-        If you don't want me to take drastic measures.`],
+        If you don't want me to take drastic measures.`
     },
     {
       trust: 'good',
       faith: 'loyal',
-      texts: [`Dear Jarl. People would like to know if you are as devoted as they are.
+      text: `Dear Jarl. People would like to know if you are as devoted as they are.
         It would be wise to do everything you can to thank the Gods.`
-      ],
     },
     {
       trust: 'loyal',
       faith: 'loyal',
-      texts: [`My dear friend. Gods are satisfied. People is blooming.
+      text: `My dear friend. Gods are satisfied. People is blooming.
         Sacrifices lead us to glory. Let's keep going.
         Come on, tell me. Who will be the next? `
-      ],
+
     },
     {
       trust: 'chaotic',
       faith: 'chaotic',
-      texts: [`Poor crazy soul. What have you done? Our people do not know who to follow.
+      text: `Poor crazy soul. What have you done? Our people do not know who to follow.
         They are lost. Show them the lights of our Gods.
         Sacrify many to avoid our souls to be lost forever. May the Gods forgive our sins.`,
-      ],
+
     },
     {
       trust: 'bad',
       faith: 'chaotic',
-      texts: [`Jarl. Are you blind? You have to act. Gods abandon us. Last events are the proof you needed.
+      text: `Jarl. Are you blind? You have to act. Gods abandon us. Last events are the proof you needed.
         Time has come to sacrifice the bravest of us. Choose wisely.`
-      ],
+
     },
     {
       trust: 'good',
       faith: 'chaotic',
-      texts: [`Dear Jarl. Despite your unbelievable efforts, it would be a pity if the Gods abandon us.
+      text: `Dear Jarl. Despite your unbelievable efforts, it would be a pity if the Gods abandon us.
         Don't forget our Gods are still watching us. Show them we still have faith.
         You're on a great way to lead our people to glory.`,
-      ],
     },
     {
       trust: 'loyal',
       faith: 'chaotic',
-      texts: [`Your greatness. A messenger of the Gods like you guides our people to the path of glory.
+      text: `Your greatness. A messenger of the Gods like you guides our people to the path of glory.
         Gods sent you signs. Even a small offering would please them.
         An old man, a diseased widow. Please my great Jarl...`
-      ],
     },
   ]
 }
