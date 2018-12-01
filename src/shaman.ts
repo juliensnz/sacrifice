d9ff539ed399c36adb8d91fb903de58e3ab5332d
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-12-01 22:58:34 +0100

    adjust messages

--- src/shaman.ts
+++ src/shaman.ts
@@ -7,61 +7,103 @@
 export default {
   shaman: [
     {
-      trust: 0,
-      faith: 75,
+      trust: 'chaotic',
+      faith: 'chaotic',
+      texts: [``, ``],
+    },
+    {
+      trust: 'bad',
+      faith: 'chaotic',
+      texts: [``, ``],
+    },
+    {
+      trust: 'good',
+      faith: 'chaotic',
+      texts: [``, ``],
+    },
+    {
+      trust: 'loyal',
+      faith: 'chaotic',
+      texts: [``, ``],
+    },
+
+
+    {
+      trust: 'chaotic',
+      faith: 'bad',
+      texts: [``, ``],
+    },
+    {
+      trust: 'bad',
+      faith: 'bad',
+      texts: [``, ``],
+    },
+    {
+      trust: 'good',
+      faith: 'bad',
+      texts: [``, ``],
+    },
+    {
+      trust: 'loyal',
+      faith: 'bad',
+      texts: [``, ``],
+    },
+    {
+      trust: 'chaotic',
+      faith: 'loyal',
       texts: [`Poor insignificant thing. Gods talked to me. You don't have what's needed to be a Jarl.
         I don't understand why but they give you a last chance. Sacrifice your people now.
         Or yourself, if you have what it takes...`
       ],
     },
     {
-      trust: 25,
-      faith: 75,
+      trust: 'bad',
+      faith: 'loyal',
       texts: [`I hear whispers. People doubt you can govern them. It would be...
         Sad, if something happened to you. Do what you have to do.
         If you don't want me to take drastic measures.`],
     },
     {
-      trust: 50,
-      faith: 75,
+      trust: 'good',
+      faith: 'loyal',
       texts: [`Dear Jarl. People would like to know if you are as devoted as they are.
         It would be wise to do everything you can to thank the Gods.`
       ],
     },
     {
-      trust: 75,
-      faith: 75,
+      trust: 'loyal',
+      faith: 'loyal',
       texts: [`My dear friend. Gods are satisfied. People is blooming.
         Sacrifices lead us to glory. Let's keep going.
         Come on, tell me. Who will be the next? `
       ],
     },
     {
-      trust: 0,
-      faith: 0,
+      trust: 'chaotic',
+      faith: 'chaotic',
       texts: [`Poor crazy soul. What have you done? Our people do not know who to follow.
         They are lost. Show them the lights of our Gods.
         Sacrify many to avoid our souls to be lost forever. May the Gods forgive our sins.`,
       ],
     },
     {
-      trust: 25,
-      faith: 0,
+      trust: 'bad',
+      faith: 'chaotic',
       texts: [`Jarl. Are you blind? You have to act. Gods abandon us. Last events are the proof you needed.
         Time has come to sacrifice the bravest of us. Choose wisely.`
       ],
     },
     {
-      trust: 50,
-      faith: 0,
+      trust: 'good',
+      faith: 'chaotic',
       texts: [`Dear Jarl. Despite your unbelievable efforts, it would be a pity if the Gods abandon us.
         Don't forget our Gods are still watching us. Show them we still have faith.
         You're on a great way to lead our people to glory.`,
       ],
     },
     {
-      trust: 75,
-      faith: 0,
+      trust: 'loyal',
+      faith: 'chaotic',
       texts: [`Your greatness. A messenger of the Gods like you guides our people to the path of glory.
         Gods sent you signs. Even a small offering would please them.
         An old man, a diseased widow. Please my great Jarl...`
--- src/villagers.ts
+++ src/villagers.ts
@@ -1,12 +1,16 @@
 export default {
   villagers: {
     trust: [
-      {score: 0, texts: [`foo`]},
-      {score: 50, texts: [`foo`, `bar`]}
+      {score: 'chaotic', texts: [`foo`]},
+      {score: 'bad', texts: [`foo`]},
+      {score: 'good', texts: [`foo`]},
+      {score: 'loyal', texts: [`foo`]},
     ],
     faith: [
-      {score: 0, texts: [`foo`]},
-      {score: 50, texts: [`foo`, `bar`]}
+      {score: 'chaotic', texts: [`foo`]},
+      {score: 'bad', texts: [`foo`]},
+      {score: 'good', texts: [`foo`]},
+      {score: 'loyal', texts: [`foo`]},
     ]
   }
 }
