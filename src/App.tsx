8c052f894619e40d089891b2d6c32494afe20c0d
pierallard <pierallard@gmail.com>
2018-12-01 14:55:18 +0100

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,20 @@
 import * as React from 'react';
 import './App.css';
 
-// import logo from './logo.svg';
-
 class App extends React.Component {
   public render() {
+    const characters = [];
+    for(let i = 0; i < 6*5; i++) {
+      characters.push({});
+    }
+
     return (
       <div className="App">
         <div className="events">
           events
         </div>
         <div className="characters">
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
-          <div className="character">Ch</div>
+          {characters.map((character) => (<div className="character">CH</div>) )}
         </div>
       </div>
     );
