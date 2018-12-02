4befdf8750cf67494b6e92de30e05e4bcafe2e27
Adrien Pétremann <hello@grena.fr>
2018-12-02 17:55:03 +0100

    Change end game conditions

--- src/core/action/cycle.ts
+++ src/core/action/cycle.ts
@@ -3,6 +3,7 @@ import {getSelectedVillagers} from 'src/core/reducer/villager';
 import parameters from 'src/core/parameters';
 import {getRandomArray} from 'src/core/utils';
 import {endGame} from 'src/core/action';
+import {Villager} from "../model";
 
 export const startCycle = () => (dispatch: any) => {
   dispatch({type: 'START_CYCLE'});
@@ -15,13 +16,35 @@ export const endCycle = () => (dispatch: any, getState: () => GameState) => {
   const numberOfSacrificed = getSelectedVillagers(getState().villagers).length;
   dispatch({type: 'PLAY_SOUND', sound: 0 === numberOfSacrificed ? 'no_sacrifice' : 'sacrifice'});
   dispatch({type: 'END_CYCLE'});
-  if (getState().cycle.number === parameters.cycleCount) {
-    dispatch(endGame());
-  } else {
-    setTimeout(() => {
-      dispatch(factAnnouncement());
-    }, 1000 * parameters.timeToDisplayAnnouncement);
-  }
+
+  const getTrust = (villagers: Villager[]) => {
+    return villagers.reduce((trust: number, villager: Villager) => {
+      return trust += villager.trust;
+    }, 0) / villagers.length;
+  };
+
+  const getFaith = (villagers: Villager[]) => {
+    return villagers.reduce((faith: number, villager: Villager) => {
+      return faith += villager.faith;
+    }, 0) / villagers.length;
+  };
+
+  const getAliveVillagers = (villagers: Villager[]) => villagers.filter((villager: Villager) => villager.alive);
+  const aliveVillagers = getAliveVillagers(getState().villagers);
+  const trustLevel = getTrust(aliveVillagers);
+  const faithLevel = getFaith(aliveVillagers);
+
+  setTimeout(() => {
+    dispatch(factAnnouncement());
+
+    if (aliveVillagers.length === 0) {
+      dispatch(endGame());
+    } else if (trustLevel === 0) {
+      dispatch(endGame());
+    } else if (faithLevel === 0) {
+      dispatch(endGame());
+    }
+  }, 1000 * parameters.timeToDisplayAnnouncement);
 };
 
 const factAnnouncement = () => (dispatch: any, getState: () => GameState) => {
