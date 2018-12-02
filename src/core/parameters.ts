bd45a1ada9593ac340e5c14ced8ed8667f6be72d
Adrien Pétremann <hello@grena.fr>
2018-12-02 21:45:31 +0100

    display always bars & less villagers

--- src/core/parameters.ts
+++ src/core/parameters.ts
@@ -14,7 +14,7 @@ const parameters = {
   cycleCount: 20,
 
   // Number of villager in the vilage
-  villagerCount: 7 * 4,
+  villagerCount: 6 * 4,
 
   // from 0 to 1: level of expressiveness (how much the villagers speaks)
   expressiveness: 0.3,
