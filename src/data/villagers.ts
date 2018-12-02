4fbdc1be82d248f7273ccefa136f2631d0927efd
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-12-02 19:14:37 +0100

    mood for villagers

--- src/data/villagers.ts
+++ src/data/villagers.ts
@@ -2,44 +2,49 @@ export default {
   villagers: {
     trust: {
       chaotic: [
-        `I have never seen a Jarl so incompetent.`,
-        `We deserve better than this false ruler.`,
-        `The time has come for a change...`,
-        `Revoooooolt!`,
-        `I heard a rebellion is in progress...`,
+        "I have never seen a Jarl so incompetent.",
+        "We deserve better than this false ruler.",
+        "The time has come for a change...",
+        "Revoooooolt!",
+        "I heard a rebellion is in progress...",
       ],
       bad: [
-        `TODO Trust bad`
+        "I'm wondering if the Jarl is capable to lead us to the path of Glory."
       ],
       good: [
-        `TODO Trust good`
+        "Our Jarl is good with us.",
       ],
       loyal: [
-        `Such a great Jarl.`,
-        `I'll pray the Gods open the gates of the Valhalla for our Jarl.`,
-        `Our ruler lead us to the path of Glory.`,
-        `Hail to the Jarl!`,
+        "Such a great Jarl.",
+        "I'll pray the Gods open the gates of the Valhalla for our Jarl.",
+        "Our ruler lead us to the path of Glory.",
+        "Hail to the Jarl!",
       ],
     },
     faith: {
       chaotic: [
-        `TODO Faith chaotic`
+        "It was a lie. All those sacrifices were useless.",
+        "I don't want to hear about those so called Gods anymore.",
+        "Odin, Freya, Thor, Baldu, Loki and all the others... Farewell.",
+        "Let's forget the Gods and live for us now.",
+        "The past if full of darkness and sacrifices. The future is bright and free."
       ],
       bad: [
-        `TODO Faith bad`
+        "Despite our sacrifices the Gods don't protect us anymore.",
       ],
       good: [
-        `Gods are good with us.`,
-        `May Odin protect my son who fights for our glory.`,
-        `May Freya protect our village.`,
-        `May the Gods be merciful with us.`,
+        "Gods are good with us.",
+        "May Odin protect my son who fights for our glory.",
+        "May Freya protect our village.",
+        "May the Gods be merciful with us.",
+        "May Ægir protect our ships heading West.",
       ],
       loyal: [
-        `I put my destiny in the hands of my Gods.`,
-        `All I do, I do it for the Gods.`,
-        `I'll fight for them. To be worthy of the Valhalla.`,
-        `If it may please the Gods, it would be a true honor to give my life for them.`,
-        `If the Gods ask for my children, it would be an honor to sacrifice them.`,
+        "I put my destiny in the hands of my Gods.",
+        "All I do, I do it for the Gods.",
+        "I'll fight for them. To be worthy of the Valhalla.",
+        "If it may please the Gods, it would be a true honor to give my life for them.",
+        "If the Gods ask for my children, it would be an honor to sacrifice them.",
       ],
     }
   }
