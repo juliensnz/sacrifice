3adbe8c4a37b6b5bdb11753ab1d99884ddf53469
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-12-03 10:37:48 +0100

    adjust villager mood

--- src/data/villagers.ts
+++ src/data/villagers.ts
@@ -14,41 +14,39 @@ export default {
       ],
       good: [
         "Our Jarl is good with us.",
-        "I trust in our Jarl.",
+        "I believe in our Jarl.",
       ],
       loyal: [
         "Such a great Jarl.",
-        "I'll pray the Gods open the gates of the Valhalla for our Jarl.",
+        "I'll pray Heimdallr opens the gates of the Valhalla for our Jarl.",
         "Our ruler lead us to the path of Glory.",
-        "Hail to the Jarl!",
+        "Long live to the Jarl!",
       ],
     },
     faith: {
       chaotic: [
         "It was a lie. All those sacrifices were useless.",
         "I don't want to hear about those so called Gods anymore.",
-        "Odin, Freya, Thor, Baldu, Loki and all the others... Farewell.",
+        "Odin, Frigg, Freya, Thor, Baldr, Loki and all the others... Farewell.",
         "Let's forget the Gods and live for us now.",
         "The past if full of darkness and sacrifices. The future is bright and free.",
-        "Our Gods are not looking at us anymore, we are lost.",
       ],
       bad: [
         "Despite our sacrifices the Gods don't protect us anymore.",
-        "Maybe our Gods are not listen to our sacrifices...",
       ],
       good: [
         "Gods are good with us.",
         "May Odin protect my son who fights for our glory.",
         "May Freya protect our village.",
         "May the Gods be merciful with us.",
-        "May Ægir protect our ships heading West.",
+        "May Ægir protect our ships heading to Birka.",
       ],
       loyal: [
         "I put my destiny in the hands of my Gods.",
-        "All I do, I do it for the Gods.",
-        "I'll fight for them. To be worthy of the Valhalla.",
-        "If it may please the Gods, it would be a true honor to give my life for them.",
-        "If the Gods ask for my children, it would be an honor to sacrifice them.",
+        "All I do, I do it for Týr.",
+        "I'll fight for Odin. To be worthy of the Valhalla.",
+        "If it may please Höðr, it would be a true honor to give my life for him.",
+        "If Frigg asks for my children, it would be an honor to sacrifice them.",
       ],
     }
   }
