f0e4f8d9b23cff7be7cb94f66c5af20299729cfa
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-12-03 12:52:05 +0100

    game message intro

--- src/data/game-messages.ts
+++ src/data/game-messages.ts
@@ -1,10 +1,16 @@
 export default {
   'intro': {
     'historic-context': [
-      ""
+      "For ages Vikings raided and traded from their Northern European homelands across wide areas of Europe.",
+      "This pagan people believed in numerous anthropomorphic Gods and Goddesses who express human emotions and in some cases are married and have children.  Among them, Odin, Frigg, Thor, Aegir, Týr, Loki, Freyja, Heimdall and Balder are some of the most well-known.",
+      "The Viking society was divided into the three socio-economic classes: Thralls, Karls and Jarls. Thralls were the lowest ranking class and were slaves. Karls were free peasants. They owned farms, land and cattle. The Jarls were the aristocracy of the Viking society. They were wealthy, owned large estates and care of administration, politics, hunting, sports, visited other Jarls or were abroad on expeditions. When a Jarl died and was buried, his household thralls were sometimes sacrificially killed and buried next to him."
     ],
     'story': [
-      ""
+      "Your father, the Jarl of the village, passed away last week. Every inhabitant saw him as the feared and respected leader he was. He lead the people to glory, opulence and wealth thanks to his sense of military strategy.",
+      "As all its predecessors, and with the help of his shaman's advises, he perpetuated the ancestral tradition aimed to please the Gods. Each new moon comes with a ritual. Each new moon comes with a price. Each new moon comes with a choice. Each new moon comes with a sacrificed...",
+      "Beware the Gods... Or they could punish you.",
+      "Keep the trust of your people... Or they will exile you.",
+      "Now you are the new Jarl of...",
     ],
   },
   'gameover': {
