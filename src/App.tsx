3a94265c66bba4af6f35eb07bf8bdaf80c03934c
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-12-03 19:36:43 +0100

    add s to second counter

--- src/App.tsx
+++ src/App.tsx
@@ -141,10 +141,10 @@ class App extends React.Component<ViewState & ViewDispatch> {
         </div>
         <div className={`App ${this.props.selectionStarted ? 'selectionPhase' : ''} ${this.props.factAnnouncement ? 'announcementPhase' : ''} ${this.props.time < 4 && !this.props.isIntro && !this.props.isLanding ? 'newDay' : ''}`}>
           <div className="sacrificeInstruction">
-            Who do you want to sacrifice? {parameters.cycleLength - this.props.time - 2 > 0 ? parameters.cycleLength - this.props.time - 3 : 0}
+            Who do you want to sacrifice? {parameters.cycleLength - this.props.time - 2 > 0 ? parameters.cycleLength - this.props.time - 3 : 0}s
           </div>
           <div className="cycleInstruction">
-            Next full moon in {parameters.cycleLength - this.props.time - parameters.selectionLength}
+            Next full moon in {parameters.cycleLength - this.props.time - parameters.selectionLength}s
           </div>
           <div className="characters">
             {this.props.villagers.map((villager: Villager) => (
